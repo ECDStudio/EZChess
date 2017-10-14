66af55f368f2d31f50b2d824c742dc7a764a9f24
ECDStudio <info@ecdevstudio.ca>
2017-10-14 15:19:55 -0700

    unit test toPosition()

---
+++ src/models/Piece.spec.js
@@ -0,0 +1,40 @@
+import { chess } from '../app';
+import Piece from './Piece';
+
+describe('toPosition', function() {
+  let piece;
+
+  beforeEach(function () {
+    piece = new Piece('white', 0, 0);
+  });
+
+  it('should change the position of the piece to [5, 5]', function() {
+    piece.toPosition(5, 5);
+    expect(piece.position.x).toEqual(5);
+    expect(piece.position.y).toEqual(5);
+  });
+
+  it('should not change the position of the piece', function() {
+    piece.toPosition(-1, 5);
+    expect(piece.position.x).toEqual(0);
+    expect(piece.position.y).toEqual(0);
+  });
+
+  it('should not change the position of the piece', function() {
+    piece.toPosition(4, 8);
+    expect(piece.position.x).toEqual(0);
+    expect(piece.position.y).toEqual(0);
+  });
+
+  it('should not change the position of the piece', function() {
+    piece.toPosition('5', 5);
+    expect(piece.position.x).toEqual(0);
+    expect(piece.position.y).toEqual(0);
+  });
+
+  it('should change the position of the piece to [-1, -1]', function() {
+    piece.toPosition(-1, -1);
+    expect(piece.position.x).toEqual(-1);
+    expect(piece.position.y).toEqual(-1);
+  });
+});
