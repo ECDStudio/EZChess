e96a65520066d58c46ba41a07bb287c008751af0
ECDStudio <info@ecdevstudio.ca>
2017-10-14 15:25:15 -0700

    test Piece

--- src/models/Piece.spec.js
+++ src/models/Piece.spec.js
@@ -1,40 +1,50 @@
 import { chess } from '../app';
 import Piece from './Piece';
 
-describe('toPosition', function() {
+describe('Piece', function() {
   let piece;
 
   beforeEach(function () {
     piece = new Piece('white', 0, 0);
   });
 
-  it('should change the position of the piece to [5, 5]', function() {
-    piece.toPosition(5, 5);
-    expect(piece.position.x).toEqual(5);
-    expect(piece.position.y).toEqual(5);
-  });
-
-  it('should not change the position of the piece', function() {
-    piece.toPosition(-1, 5);
+  it('should create an instance of a piece', function() {
+    expect(piece.side).toEqual('white');
     expect(piece.position.x).toEqual(0);
     expect(piece.position.y).toEqual(0);
+    expect(piece.step).toEqual(0);
   });
 
-  it('should not change the position of the piece', function() {
-    piece.toPosition(4, 8);
-    expect(piece.position.x).toEqual(0);
-    expect(piece.position.y).toEqual(0);
-  });
+  describe('toPosition', function() {
 
-  it('should not change the position of the piece', function() {
-    piece.toPosition('5', 5);
-    expect(piece.position.x).toEqual(0);
-    expect(piece.position.y).toEqual(0);
-  });
+    it('should change the position of the piece to [5, 5]', function() {
+      piece.toPosition(5, 5);
+      expect(piece.position.x).toEqual(5);
+      expect(piece.position.y).toEqual(5);
+    });
 
-  it('should change the position of the piece to [-1, -1]', function() {
-    piece.toPosition(-1, -1);
-    expect(piece.position.x).toEqual(-1);
-    expect(piece.position.y).toEqual(-1);
+    it('should not change the position of the piece', function() {
+      piece.toPosition(-1, 5);
+      expect(piece.position.x).toEqual(0);
+      expect(piece.position.y).toEqual(0);
+    });
+
+    it('should not change the position of the piece', function() {
+      piece.toPosition(4, 8);
+      expect(piece.position.x).toEqual(0);
+      expect(piece.position.y).toEqual(0);
+    });
+
+    it('should not change the position of the piece', function() {
+      piece.toPosition('5', 5);
+      expect(piece.position.x).toEqual(0);
+      expect(piece.position.y).toEqual(0);
+    });
+
+    it('should change the position of the piece to [-1, -1]', function() {
+      piece.toPosition(-1, -1);
+      expect(piece.position.x).toEqual(-1);
+      expect(piece.position.y).toEqual(-1);
+    });
   });
 });
