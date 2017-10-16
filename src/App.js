c01f093f723f24dd7141e9190a61799368013847
ECDStudio <info@ecdevstudio.ca>
2017-10-15 23:34:49 -0700

    copy models from original dir

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chess from './models/Chess';
 
 class App extends Component {
+  chess = new Chess();
+
   render() {
+    let currentTurn;
+
+    for (let player in this.chess.players) {
+      if (this.chess.players[player].isTurn === true) {
+        currentTurn = this.chess.players[player].side;
+      }
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="chess-container">
+        <div className="chessboard">
+        
+        </div>
+        <div>
+          Current Turn: {currentTurn}
+        </div>
       </div>
     );
   }
