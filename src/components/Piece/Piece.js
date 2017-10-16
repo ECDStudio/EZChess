05d832f063798fec55a7eb1bbda95ad10be8744a
ECDStudio <info@ecdevstudio.ca>
2017-10-16 03:40:13 -0700

    working on piece states

--- src/components/Piece/Piece.js
+++ src/components/Piece/Piece.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
 
 class Piece extends Component {
+  state = {
+    style: {
+      left: `${this.props.model.position.x * 12.5}%`,
+      bottom: `${this.props.model.position.y * 12.5}%`,
+    }
+  };
+  update = () => {
+    this.setState(prevState => ({
+      style: {
+        zIndex: 20,
+        left: `${this.props.model.position.x * 12.5}%`,
+        bottom: `${this.props.model.position.y * 12.5}%`,
+      }
+    }));
+    setTimeout(() => {
+      this.setState(prevState => ({
+        style: {
+          zIndex: 1,
+          left: `${this.props.model.position.x * 12.5}%`,
+          bottom: `${this.props.model.position.y * 12.5}%`,
+        }
+      }));
+    }, 1000);
+  };
+  
+  componentWillUpdate() {
+    console.log('123')
+  }
+
   clearTargets() {
     // Remove all pieces' selectable targets in the DOM
     for (let i of document.getElementsByClassName('target-position')) {
@@ -21,12 +50,13 @@ class Piece extends Component {
       target.classList.add('target-position');
       target.style.left = `${position[0] * 12.5}%`;
       target.style.bottom = `${position[1] * 12.5}%`;
-      target.addEventListener('click', () => this.toPosition(game, model, position));
+      target.addEventListener('click', () => this.toPosition(model, position, game));
     }
   }
 
-  toPosition(game, piece, position) {
+  toPosition(piece, position, game) {
     piece.toPosition(game, ...position);
+    this.update();
     this.clearTargets();
     for (let player in game.players) {
       for (let p in game.players[player].pieces) {
@@ -42,18 +72,15 @@ class Piece extends Component {
     }
     // Pass the turn to the other player
     game.switchTurn();
+    return game;
   }
 
   render() {
-    let style = {
-      left: `${this.props.model.position.x * 12.5}%`,
-      bottom: `${this.props.model.position.y * 12.5}%`
-    }
-
     return (
       <li className={`chess-piece ${this.props.model.side} ${this.props.model.class}`}
-          style={style}
-          onClick={() => this.availableMoves(this.props.model, this.props.game)}></li>
+          style={this.state.style}
+          onClick={() => this.availableMoves(this.props.model, this.props.game)}>
+      </li>
     )
   }
 }
