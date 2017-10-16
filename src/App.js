5168c734cd810379595179cb32f7b589a963c130
ECDStudio <info@ecdevstudio.ca>
2017-10-16 00:20:20 -0700

    file managements

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Chess from './models/Chess';
+import TurnIndicator from './TurnIndicator/TurnIndicator';
 
 class App extends Component {
   chess = new Chess();
 
   render() {
-    let currentTurn;
-
-    for (let player in this.chess.players) {
-      if (this.chess.players[player].isTurn === true) {
-        currentTurn = this.chess.players[player].side;
-      }
-    }
     return (
       <div className="chess-container">
         <div className="chessboard">
-        
+          <ul>
+          </ul>
+          <ul>
+          </ul>
         </div>
-        <div>
-          Current Turn: {currentTurn}
-        </div>
+        <TurnIndicator game={this.chess} />
       </div>
     );
   }
---
+++ src/TurnIndicator/TurnIndicator.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class TurnIndicator extends Component {
+  render() {
+    const game = this.props.game;
+    let currentTurn;
+  
+    for (let player in game.players) {
+      if (game.players[player].isTurn === true) {
+        currentTurn = game.players[player].side;
+      }
+    }
+    return (
+      <div>
+        Current Turn: {currentTurn}
+      </div>
+    );
+  }
+}
+
+export default TurnIndicator;
